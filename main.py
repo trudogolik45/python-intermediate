8c6cf5e36e17a6139bb97ea8b80f27392149b864
trudogolik45 <210735425+trudogolik45@users.noreply.github.com>
2026-07-25 20:57:47 +0300

    feat(users): базовый класс пользователя, наследники и UserManager

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, APIRouter
+from users.views import user_router
 
 from products.views import products_router
 
@@ -7,4 +8,5 @@ app = FastAPI()
 
 api_v1_router = APIRouter(prefix="/v1/api")
 api_v1_router.include_router(products_router)
+api_v1_router.include_router(user_router)
 app.include_router(api_v1_router)
